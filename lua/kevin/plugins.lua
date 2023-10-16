7c1e1d71cfbbcafaf3b01763b6807415f7a38eb1
Your Name <you@example.com>
2023-10-16 10:17:42 +0200

    git signs + status bar

--- lua/kevin/plugins.lua
+++ lua/kevin/plugins.lua
@@ -46,5 +46,6 @@ require("lazy").setup({
 	{ "nvim-tree/nvim-web-devicons" },
 	{ "akinsho/bufferline.nvim", config = function() require("bufferline").setup() end },
 	{ "numToStr/Comment.nvim", config = function () require("Comment").setup() end },
-	{ "MaxMEllon/vim-jsx-pretty" }
+	{ "lewis6991/gitsigns.nvim", config = function() require("gitsigns").setup() end },
+	{ "nvim-lualine/lualine.nvim", config = function() require("lualine").setup() end }
 })

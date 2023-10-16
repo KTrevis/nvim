fa93160eb52a990bc11ba9a6980cc708256ef45a
ketrevis <ketrevis@42.student.fr>
2023-10-16 18:43:24 +0200

    tree on leader e and grep on fg

--- lua/kevin/remap.lua
+++ lua/kevin/remap.lua
@@ -6,9 +6,9 @@ vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("n", ";", ":")
 
-vim.keymap.set("n", "<Leader>t", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<Leader>e", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<Leader>ff", ":Telescope find_files<CR>")
-vim.keymap.set("n", "<Leader>fw", ":Telescope live_grep<CR>")
+vim.keymap.set("n", "<Leader>fg", ":Telescope live_grep<CR>")
 vim.keymap.set("n", "<Leader>fc", ":Telescope current_buffer_fuzzy_find<CR>")
 vim.keymap.set("n", "<Leader>fb", ":Telescope buffers<CR>")
 

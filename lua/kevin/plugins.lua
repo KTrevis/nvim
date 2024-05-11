761f141107c64e7a5a264122317226d9a3503d36
Kevin Trevis <ketrevis@bess-f4r1s3.clusters.42paris.fr>
2024-05-11 11:13:51 +0200

    now using toggleterm instead of floaterm

--- lua/kevin/plugins.lua
+++ lua/kevin/plugins.lua
@@ -12,33 +12,24 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.3',
-		dependencies = {'nvim-lua/plenary.nvim'}
-	},
+	{ 'nvim-telescope/telescope.nvim', tag = '0.1.3', dependencies = {'nvim-lua/plenary.nvim'} },
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 	{'williamboman/mason.nvim'},
 	{'williamboman/mason-lspconfig.nvim'},
-	{
-		'neovim/nvim-lspconfig',
-		dependencies = {'hrsh7th/cmp-nvim-lsp'},
-  },
-  { 'hrsh7th/nvim-cmp',
-		dependencies = {'L3MON4D3/LuaSnip'}
-	},
+	{ 'neovim/nvim-lspconfig', dependencies = {'hrsh7th/cmp-nvim-lsp'}, },
+	{ 'hrsh7th/nvim-cmp', dependencies = {'L3MON4D3/LuaSnip'} },
 	{ "catppuccin/nvim" },
-	{ "voldikss/vim-floaterm" },
 	{
 		"Diogo-ss/42-header.nvim",
 		lazy = false,
 		config = function()
-		local header = require("42header")
-		header.setup({
-			default_map = true, -- default Mapping <F1> in normal mode
-			auto_update = true,  -- update header when saving
-			user = "ketrevis", -- your user
-			mail = "ketrevis@student.42.fr", -- your mail
-		})
+			local header = require("42header")
+			header.setup({
+				default_map = true, -- default Mapping <F1> in normal mode
+				auto_update = true,  -- update header when saving
+				user = "ketrevis", -- your user
+				mail = "ketrevis@student.42.fr", -- your mail
+			})
 		end
 	},
 	{ "nvim-tree/nvim-web-devicons" },
@@ -47,5 +38,6 @@ require("lazy").setup({
 	{ "nvim-lualine/lualine.nvim", config = function() require("lualine").setup() end },
 	{ "m4xshen/autoclose.nvim", config = function() require("autoclose").setup() end },
 	{ "terrortylor/nvim-comment", config = function() require("nvim_comment").setup() end },
-	{ "nvim-tree/nvim-tree.lua", config = function() require("nvim-tree").setup() end }
+	{ "nvim-tree/nvim-tree.lua", config = function() require("nvim-tree").setup() end },
+	{ "akinsho/toggleterm.nvim", config = function() require("toggleterm").setup() end },
 })

e860665a195782e7e4619bad99a9ef30ac4093f5
galadrim <galadrim@MacBook-Pro-de-galadrim.local>
2025-03-18 17:36:32 +0100

    rebind remap

--- lua/kevin/remap.lua
+++ lua/kevin/remap.lua
@@ -5,8 +5,8 @@ vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<S-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<S-j>", ":m '>+1<CR>gv=gv")
 
-vim.keymap.set("v", "<C-_>", ":CommentToggle<CR>")
-vim.keymap.set("n", "<C-_>", ":CommentToggle<CR>")
+vim.keymap.set("v", "?", ":CommentToggle<CR>")
+vim.keymap.set("n", "?", ":CommentToggle<CR>")
 
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("n", ";", ":")
@@ -16,10 +16,6 @@ vim.keymap.set("n", "<Leader>ff", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<Leader>fg", ":Telescope live_grep<CR>")
 vim.keymap.set("n", "<Leader>fc", ":Telescope current_buffer_fuzzy_find<CR>")
 vim.keymap.set("n", "<Leader>fb", ":Telescope buffers<CR>")
-
-vim.keymap.set("t", "<A-n>", "norminette srcs/ | grep -v OK<CR>")
-vim.keymap.set("t", "<A-g>", "lazygit<CR>")
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 vim.keymap.set("n", "<Leader>fh", vim.lsp.buf.hover)
 vim.keymap.set("n", "<Leader>fd", vim.lsp.buf.definition)
